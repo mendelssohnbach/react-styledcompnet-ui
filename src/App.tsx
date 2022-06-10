3b871a954ec0a102e6256023ed785f580755be1a
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2022-06-10 13:23:29 +0900

    :lipstick: Heading UI作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Heading } from './libs/constants/Heading';
 import { Text } from './libs/Text';
 
 export const App = () => {
-  return <Text text={true} />;
+  return (
+    <>
+      <Text text="true" />
+      <Heading tag="h1">見出し</Heading>
+      <Heading tag="h2">
+        <span>hello, world!</span>
+      </Heading>
+    </>
+  );
 };
